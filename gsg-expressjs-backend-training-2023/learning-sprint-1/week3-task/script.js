38ce625c81a3044e52b20c708d69ba2acea51ecb
Mohammad Khaled Mohammad Odah <137823341+MKOdeh2024@users.noreply.github.com>
2023-07-03 14:13:16 +0300

    Update script.js

--- gsg-expressjs-backend-training-2023/learning-sprint-1/week3-task/script.js
+++ gsg-expressjs-backend-training-2023/learning-sprint-1/week3-task/script.js
@@ -1,15 +1,40 @@
-async function fetchData() {
+// script.js
+async function fetchAliveCharacters() {
   try {
-    const response = await fetch('https://rickandmortyapi.com/api/character/?status=alive');
-    
+    const response = await fetch('https://rickandmortyapi.com/api/character?status=alive');
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    
+
     const data = await response.json();
-    return data.filter(object => object.length > 50).map(object => object.slice(0,50);
+    return data.results.slice(0, 50);
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
   }
 }
+
+async function displayAliveCharacters() {
+  try {
+    const characterList = document.getElementById('character-list');
+    const characters = await fetchAliveCharacters();
+
+    characters.forEach(character => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <h3>${character.name}</h3>
+        <img src="${character.image}" alt="${character.name}">
+        <p>Location: ${character.location.name}</p>
+        <p>Species: ${character.species}</p>
+        <p>Gender: ${character.gender}</p>
+      `;
+      characterList.appendChild(li);
+    });
+  } catch (error) {
+    const characterList = document.getElementById('character-list');
+    characterList.innerHTML = '<p>Error fetching characters. Please try again later.</p>';
+    console.error('Error:', error);
+  }
+}
+
+displayAliveCharacters();
