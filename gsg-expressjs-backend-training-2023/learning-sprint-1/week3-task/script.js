4cb78dac30329268979ba4b6a791c826f646d258
Mohammad Khaled Mohammad Odah <137823341+MKOdeh2024@users.noreply.github.com>
2023-07-03 14:09:06 +0300

    Update script.js

--- gsg-expressjs-backend-training-2023/learning-sprint-1/week3-task/script.js
+++ gsg-expressjs-backend-training-2023/learning-sprint-1/week3-task/script.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+async function fetchData() {
+  try {
+    const response = await fetch('https://rickandmortyapi.com/api/character/?status=alive');
+    
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
+    const data = await response.json();
+    return data.filter(object => object.length > 50).map(object => object.slice(0,50);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
